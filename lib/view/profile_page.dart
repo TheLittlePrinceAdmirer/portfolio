8c59c95c016e5f31702d64adc50a293fea69fa77
apple <koyo272@icloud.com>
2024-02-29 16:51:57 +0900

    initialised

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,4 +1,4 @@
-import 'package:csv_spread_sheets_demo/view/login_page.dart';
+import 'package:portfolio/view/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
